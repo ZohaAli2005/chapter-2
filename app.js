5f5302e9f1ac19fd97d755623ca89bceb612e0dc
ZOHA ALI <zoha97069@gmail.com>
2026-05-30 09:49:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+// Problem 1
+
+let username;
+
+// Problem 2
+
+let myName = "Zoha Ali";
+
+// Problem 3
+
+//a
+let message;
+//b
+message = "Hello World";
+//c
+alert(message);
+
+// Problem 4
+
+let studentName = 'Jhone Doe', studentAge = '15 year old' , studentCourse = 'Modern Web & App Development';
+alert(studentName); alert(studentAge); alert(studentCourse);
+
+// Problem 5
+
+let food = 'PIZZA\nPIZZ\nPIZ\nPI\nP';
+alert(food);
+
+// Problem 6
+
+let email = 'zohaali7096@gmail.com';
+alert('My email adress is ' + email);
+
+// Problem 7
+
+let book = 'A smarter way to learn JavaScript';
+alert('I am trying to learn from the Book' + book);
+
+// Problem 8
+
+document.writeln('Yah! I can write html content through Javascript.');
+
+// Problem 09
+
+let sign = '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬';
+alert(sign);
